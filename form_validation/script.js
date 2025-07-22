b4567bfaa4b061ca381f769036ab58a528918032
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-07-22 10:30:06 +0530

    update

---
+++ form_validation/script.js
@@ -0,0 +1,50 @@
+let submitbtn = ()=>{
+    let Name = document.querySelector("#name").value.trim()
+    let Num = document.querySelector("#number").value.trim()
+    let Email = document.querySelector("#email").value.trim()
+    let Pass = document.querySelector("#password").value.trim()
+    let Cpass = document.querySelector("#confirmPassword").value.trim()
+
+    let Errname = document.querySelector(".errename")
+    let Errnum = document.querySelector(".errorenum")
+    let Errmail = document.querySelector(".erroremail")
+    let Errpass = document.querySelector(".errorpass")
+    let Errcpass = document.querySelector(".errorcpass")
+
+    if(Name == ""){
+        Errname.innerText = "Please Fill Your Name"
+        Errname.style.visibility = "visible"
+        return false
+    }
+    else if( isNaN(Num) ){
+        Errnum.innerText = "Please Enter Numbers only"
+        Errnum.style.visibility = "visible"
+        return false    
+    }
+    else if( Num.length != 10){
+        Errnum.innerText = "Please Enter Valid Number"
+        Errnum.style.visibility = "visible"
+        return false
+    }
+    else if(Email == ""){
+        Errmail.innerText = "Please Enter Your Email"
+        Errmail.style.visibility = "visible"     
+        return false
+    }
+    else if(Pass == ""){
+        Errpass.innerText = "Please Enter Password"
+        Errpass.style.visibility = "visible"     
+        return false
+    }
+    else if(Cpass == ""){
+        Errcpass.innerText = "Please Enter Confirm Password"
+        Errcpass.style.visibility = "visible"     
+        return false
+    }
+    else if(Cpass != Pass){
+        Errcpass.innerText = "Please Enter Correct Confirm Password"
+        Errcpass.style.visibility = "visible"     
+        return false
+    }
+    
+}
