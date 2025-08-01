34c3d47dc5e1c5f1965a4353d971b65566a7b358
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-08-02 00:59:24 +0530

    update

---
+++ stop_watch/script.js
@@ -0,0 +1,19 @@
+let screen = document.querySelector(".timer")
+
+let seconds = 0
+let mins = 0
+let hours = 0
+let startTimer = ()=>{
+    setInterval(()=>{
+        screen.innerHTML = `${hours} : ${mins} : ${seconds}`
+        if(seconds == 59){
+            mins = mins + 1
+            seconds = 0
+        }
+        if(mins == 59){
+            hours = hours + 1
+            mins = 0
+        }
+        seconds = seconds + 1
+    },1000)
+}
